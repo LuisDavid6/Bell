fb7d05c4023e93fe27a6dc415925545b95a5f5b4
David <luisdavid.0698@hotmail.com>
2023-12-05 14:10:27 -0500

    feat: add favicon

--- app/(user)/foods/page.tsx
+++ app/(user)/foods/page.tsx
@@ -8,7 +8,7 @@ const Foods = async () => {
     <div className='w-full flex flex-col justify-center items-center'>
       <h3 className='text-title font-bold text-2xl sm:text-3xl italic my-5'>Comidas</h3>
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 mx-3 w-5/6 mb-5'>
-        <FoodsComponent foods={foods} />h
+        <FoodsComponent foods={foods} />
       </div>
     </div>
   )
