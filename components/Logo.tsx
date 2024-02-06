7c63fd1a32853029f444b080c72c0ef247877065
David <luisdavid.0698@hotmail.com>
2024-02-05 23:36:33 -0500

    fix: fix dark mode logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,9 +2,12 @@
 import Image from 'next/image'
 import logo from '@/assets/images/logo.png'
 import logoDark from '@/assets/images/logo2.png'
+import { useTheme } from 'next-themes'
 
 const Logo = ({ width, height }: { width: number; height: number }) => {
-  return <Image src={localStorage ? (localStorage.getItem('theme') === 'light' ? logo : logoDark) : ''} alt='logo' width={width} height={height} />
+  const { theme } = useTheme()
+
+  return <Image src={theme === 'light' ? logo : logoDark} alt='logo' width={width} height={height} />
 }
 
 export default Logo
